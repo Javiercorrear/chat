b548de297805ebb0bc414b4c2b8180b169589ccf
Javier Correa <javier.correa@taglivros.com.br>
2021-07-05 12:10:14 -0300

    chore(package): remove unnused dependency.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require( 'express' )
 const path = require( 'path' )
 const ejs = require( 'ejs' )
-// const cors = require( 'cors' )
 
 const app = express()
 const server = require( 'http' ).createServer( app )
@@ -9,15 +8,14 @@ const io = require( 'socket.io' )( server, { cors: { origin: '*' } } )
 
 const PORT = process.env.PORT || 3000
 
-// app.use( cors() )
-// app.use( express.static( path.join( __dirname, 'public' ) ) )
-// app.set( 'views', path.join( __dirname, 'public' ) )
-// app.engine( 'html', ejs.renderFile )
-// app.set( 'view engine', 'html' )
+app.use( express.static( path.join( __dirname, 'public' ) ) )
+app.set( 'views', path.join( __dirname, 'public' ) )
+app.engine( 'html', ejs.renderFile )
+app.set( 'view engine', 'html' )
 
-// app.use( '/', ( req, res ) => {
-//     res.render( 'index.html' )
-// } )
+app.use( '/', ( req, res ) => {
+    res.render( 'index.html' )
+} )
 
 const messages = []
 
