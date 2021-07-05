e6c5b16c34d41e1ea5805337fb65a925363fd932
Javier Correa <javier.correa@taglivros.com.br>
2021-07-05 10:30:26 -0300

    feat(package): add start script.

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 const server = require( 'http' ).createServer( app )
 const io = require( 'socket.io' )( server )
 
+const PORT = 3000
+
 app.use( express.static( path.join( __dirname, 'public' ) ) )
 app.set( 'views', path.join( __dirname, 'public' ) )
 app.engine( 'html', ejs.renderFile )
@@ -28,4 +30,5 @@ io.on( 'connection', socket => {
   } )
 } )
 
-server.listen( 3000 )
+console.log( `Server running on port ${ PORT }` )
+server.listen( PORT )
